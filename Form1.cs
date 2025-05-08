f9634ffda30710f8394113734a9b49dbbbf57765
Yasin Cüre <148881966+yasindenizcure@users.noreply.github.com>
2025-05-08 18:00:46 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,16 @@
-﻿using CSharpEgitimKampi501.Dtos;
-using Dapper;
+﻿using CsharpEgitimKampi601.Entities;
+using CsharpEgitimKampi601.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Markup;
 
-namespace CSharpEgitimKampi501
+namespace CsharpEgitimKampi601
 {
     public partial class Form1 : Form
     {
@@ -21,70 +19,59 @@ namespace CSharpEgitimKampi501
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-8TRT9BV\\MSSQLSERVER01;Initial Catalog=EgitimKampi501Db;integrated security=true");
-        private async void btnList_Click(object sender, EventArgs e)
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
         {
-            string query = "Select * from TblProduct";
-            var values = await connection.QueryAsync<ResultProductDto>(query);
-            dataGridView1.DataSource = values;
+            var customer = new Customer()
+            {
+              CustomerName = txtCustomerName.Text,
+              CustomerSurname = txtCustomerSurname.Text,
+              CustomerCity = txtCustomerCity.Text,
+              CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+              CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text)
+            };
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Müşteri Ekleme İşlemi Başarılı", "uyarı", MessageBoxButtons.OK,MessageBoxIcon.Information);
         }
 
-        private async void btnAdd_Click(object sender, EventArgs e)
+        private void btnCustomerList_Click(object sender, EventArgs e)
         {
-            string query = "insert into TblProduct(ProductName,ProductStock,ProductPrice,ProductCategory) values (@productName,@productStock,@productPrice,@productCategory)";
-            var parameters = new DynamicParameters();
-            parameters.Add("@productName", txtProductName.Text);
-            parameters.Add("@productStock", txtProductStock.Text);
-            parameters.Add("@productPrice", txtProductPrice.Text);
-            parameters.Add("@productCategory", txtProductCategory.Text);
-            await connection.ExecuteAsync(query, parameters);
-            MessageBox.Show("Yeni Kitap Ekleme Başarılı.");
+            List<Customer> customer = customerOperations.GetAllCustomers();
+            dataGridView1.DataSource = customer;
         }
 
-        private async void btnDelete_Click(object sender, EventArgs e)
+        private void btnCustomerDelete_Click(object sender, EventArgs e)
         {
-            string query = "delete from TblProduct where ProductId = @productId";
-            var parameters= new DynamicParameters();
-            parameters.Add("@productId", txtProductId.Text);
-            await connection.ExecuteAsync(query, parameters);
-            MessageBox.Show("Kitap Silme Başarılı.");
+            string customerId = txtCustomerId.Text;
+            customerOperations.DeleteCustomer(customerId);
+            MessageBox.Show("Müşteri Başarıyla Silindi!");
         }
 
-        private async void btnUpdate_Click(object sender, EventArgs e)
+        private void btnCustomerUpdate_Click(object sender, EventArgs e)
         {
-            string query = "update TblProduct set ProductName = @productName, ProductStock = @productStock, ProductPrice = @productPrice, ProductCategory = @productCategory where ProductId = @productId";
-            var parameters = new DynamicParameters();
-            parameters.Add("@productName", txtProductName.Text);
-            parameters.Add("@productStock", txtProductStock.Text);
-            parameters.Add("@productPrice", txtProductPrice.Text);
-            parameters.Add("@productCategory", txtProductCategory.Text);
-            parameters.Add("@productId", txtProductId.Text);
-            await connection.ExecuteAsync(query, parameters);
-            MessageBox.Show("Kitap Güncelleme Başarılı.","Güncelleme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            string id = txtCustomerId.Text;
+            var updateCustomer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text),
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerId = id
+            };
+            customerOperations.UpdateCustomer(updateCustomer);
+            MessageBox.Show("Müşteri Başarıyla Güncellendi!");
         }
 
-        private async void Form1_Load(object sender, EventArgs e)
+        private void btnGetByCustomerId_Click(object sender, EventArgs e)
         {
-            string query1 = "select count(*) from TblProduct";
-            var productTotalCount = await connection.QueryFirstOrDefaultAsync<int>(query1);
-            lblTotalProductCount.Text = productTotalCount.ToString();
-
-            string query2= "Select ProductName from TblProduct where ProductPrice= (select max(ProductPrice) from TblProduct)";
-            var maxProductPriceName = await connection.QueryFirstOrDefaultAsync<string>(query2);
-            lblMaxPriceProductName.Text = maxProductPriceName.ToString();
-
-            string query3= "Select Count(Distinct ProductCategory) from TblProduct";
-            var DisctintProductCount = await connection.QueryFirstOrDefaultAsync<int>(query3);
-            lblDistinctCategoryCount.Text = DisctintProductCount.ToString();
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-
-        }
+            string id = txtCustomerId.Text;
+            Customer customers = customerOperations.GetCustomerById(id);
+            dataGridView1.DataSource = new List<Customer> {customers};
+        }   
     }
 }
